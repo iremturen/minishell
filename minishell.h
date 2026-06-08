f623eaa00785aa24844bfcb9a1c50d1e67388dc9
azrakarakaya1 <azrakarakaya09@gmail.com>
2026-06-08 22:07:57 +0300

    add: structs and prototypes organized by layer

--- minishell.h
+++ minishell.h
@@ -4,78 +4,125 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <sys/wait.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "Libft/libft.h"
 
-//********************** 
+/* ══════════════════════════════════════════════════════
+**  ENUM'LAR
+** ══════════════════════════════════════════════════════ */
+
 typedef enum e_token_type
 {
-	TOK_WORD, // < < | >> << ' " dışındaki her şey
-	TOK_PIPE, // |
-	TOK_REDIR_IN, // <
-	TOK_REDIR_OUT, // >
-	TOK_APPEND, // >>
-	TOK_HEREDOC // <<
-}t_token_type;
-
-typedef struct s_token
-{
-	char			*value;
-	t_token_type	type;
-	struct s_token	*next;
-}t_token;
-//********************** 
-//********************** 
-
-typedef struct s_redir
-{
-	char			*file; //redirs deki file ı tutar
-	char			*delimiter;
-	int				fd;
-	t_token_type	type;
-	struct s_redir	*next;
-}t_redir;
-
-typedef struct s_cmd
-{
-	char			**argv; //komutun ismi ve yolu
-	char			*cmd_path;
-	int				is_builtin;
-	t_redir			*redirs; // cat < file 
-	struct s_cmd	*next;
-}t_cmd;
-//********************** 
-
+	TOK_WORD,		// normal kelime: ls, -la, filename.txt
+	TOK_PIPE,		// |
+	TOK_REDIR_IN,	// <
+	TOK_REDIR_OUT,	// >
+	TOK_APPEND,		// >>
+	TOK_HEREDOC		// <<
+}	t_token_type;
 
 typedef enum e_quote_state
 {
 	Q_NONE,
 	Q_SINGLE,
 	Q_DOUBLE
-}t_quote_state;
+}	t_quote_state;
 
+/* ══════════════════════════════════════════════════════
+**  STRUCT'LAR
+** ══════════════════════════════════════════════════════ */
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
+
+typedef struct s_redir
+{
+	char			*file;
+	char			*delimiter;
+	int				fd;
+	t_token_type	type;
+	struct s_redir	*next;
+}	t_redir;
+
+typedef struct s_cmd
+{
+	char			**argv;
+	char			*cmd_path;
+	int				is_builtin;
+	t_redir			*redirs;
+	struct s_cmd	*next;
+}	t_cmd;
 
 typedef struct s_shell
 {
 	char	**envp;
 	int		last_exit;
-}t_shell;
+}	t_shell;
 
-int		is_operator(char c);
-char	**split_inputs(char *line);
-size_t	get_spaced_len(char *line);
-int		print_syntax_error(const char *token);
-int		validate_lexer_syntax(char **args);
+/* ══════════════════════════════════════════════════════
+**  INIT
+** ══════════════════════════════════════════════════════ */
 
-void	execute_cmd(t_cmd *cmd, t_shell *shell);
-int		is_builtin(const char *cmd);
-void	execute_builtin(t_cmd *cmd, t_shell *shell);
-void	execute_single(t_cmd *cmd, char **envp);
+t_shell		*init_shell(char **envp);
+void		free_shell(t_shell *shell);
 
-char	**find_path(char **envp);
-char	*find_command(char **paths, char *cmd);
-char	*resolve_path(char *cmd, char **envp);
-void	free_array(char **arr);
+/* ══════════════════════════════════════════════════════
+**  LEXER
+** ══════════════════════════════════════════════════════ */
+
+t_token		*tokenize(char *line);
+void		free_tokens(t_token *head);
+
+int			is_operator(char c);
+size_t		get_spaced_len(char *line);
+char		*add_spaces(char *line);
+char		**split_inputs(char *line);
+int			validate_lexer_syntax(char **args);
+int			print_syntax_error(const char *token);
+
+/* ══════════════════════════════════════════════════════
+**  PARSER
+** ══════════════════════════════════════════════════════ */
+
+t_cmd		*parse(t_token *tokens);
+void		free_cmds(t_cmd *head);
+
+/* ══════════════════════════════════════════════════════
+**  EXPANDER
+** ══════════════════════════════════════════════════════ */
+
+void		expand_tokens(t_token *head, t_shell *shell);
+void		handle_quotes(t_token *head);
+
+/* ══════════════════════════════════════════════════════
+**  EXECUTOR
+** ══════════════════════════════════════════════════════ */
+
+void		execute_cmd(t_cmd *cmd, t_shell *shell);
+void		execute_builtin(t_cmd *cmd, t_shell *shell);
+void		execute_single(t_cmd *cmd, char **envp);
+
+char		**find_path(char **envp);
+char		*find_command(char **paths, char *cmd);
+char		*resolve_path(char *cmd, char **envp);
+void		free_array(char **arr);
+
+/* ══════════════════════════════════════════════════════
+**  BUILTINS
+** ══════════════════════════════════════════════════════ */
+
+int			is_builtin(char *cmd);
+void		builtin_cd(t_cmd *cmd, t_shell *shell);
+void		builtin_echo(char **argv);
+void		builtin_pwd(void);
+void		builtin_env(char **envp);
+void		builtin_unset(t_cmd *cmd, t_shell *shell);
+void		builtin_exit(t_shell *shell);
 
 #endif
