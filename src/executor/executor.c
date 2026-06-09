7cd014e775508921ad815db4d3a814aca76b92df
azrakarakaya1 <azrakarakaya09@gmail.com>
2026-06-10 02:39:49 +0300

    fix: executor norm compliance and route to execute_single with shell

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,15 +1,15 @@
 #include "../../minishell.h"
 
-void execute_cmd(t_cmd *cmd, t_shell *shell)
+// cmd->next varsa ileride pipeline a yonlendirilecek
+// simdilik sadece ilk komutu calistiriyor
+void	execute_cmd(t_cmd *cmd, t_shell *shell)
 {
-    if (!cmd || !cmd->argv || !cmd->argv[0])
-        return;
-
-    if (is_builtin(cmd->argv[0]))
-    {
-        execute_builtin(cmd, shell);
-        return;
-    }
-
-    execute_single(cmd, shell->envp);
+	if (!cmd || !cmd->argv || !cmd->argv[0])
+		return ;
+	if (is_builtin(cmd->argv[0]))
+	{
+		execute_builtin(cmd, shell);
+		return ;
+	}
+	execute_single(cmd, shell);
 }
