257e83f49392946cdd663f806874788f5a078238
azrakarakaya1 <azrakarakaya09@gmail.com>
2026-06-11 14:48:56 +0300

    add: route to execute_pipeline when multiple commands

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,11 +1,15 @@
 #include "../../minishell.h"
 
-// cmd->next varsa ileride pipeline a yonlendirilecek
-// simdilik sadece ilk komutu calistiriyor
+// cmd->next varsa pipeline a yonlendir, yoksa tek komut calistir
 void	execute_cmd(t_cmd *cmd, t_shell *shell)
 {
 	if (!cmd || !cmd->argv || !cmd->argv[0])
 		return ;
+	if (cmd->next)
+	{
+		execute_pipeline(cmd, shell);
+		return ;
+	}
 	if (is_builtin(cmd->argv[0]))
 	{
 		execute_builtin(cmd, shell);
