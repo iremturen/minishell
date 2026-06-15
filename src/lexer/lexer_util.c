ee540feb4ab089340194dc013e11c1fe5012b871
azrakarakaya1 <azrakarakaya09@gmail.com>
2026-06-15 20:34:09 +0300

    add: has_unclosed_quotes check, syntax error on unmatched quotes

--- src/lexer/lexer_util.c
+++ src/lexer/lexer_util.c
@@ -33,3 +33,26 @@ size_t	get_spaced_len(char *line)
 	}
 	return (len + 1);
 }
+
+// tirnaklarin kapanip kapanmadigini kontrol ediyor, acik kalirsa 1 doner
+int	has_unclosed_quotes(char *line)
+{
+	int	q;
+	int	i;
+
+	q = Q_NONE;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\'' && q == Q_NONE)
+			q = Q_SINGLE;
+		else if (line[i] == '\'' && q == Q_SINGLE)
+			q = Q_NONE;
+		else if (line[i] == '"' && q == Q_NONE)
+			q = Q_DOUBLE;
+		else if (line[i] == '"' && q == Q_DOUBLE)
+			q = Q_NONE;
+		i++;
+	}
+	return (q != Q_NONE);
+}
